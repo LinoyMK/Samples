5a39bbe236e0e14f58e2dbaafc33904a2a51be2a
linoy <linoymk@gmail.com>
2016-09-01 23:27:37 +0530

    Added String & StringBuilder

---
+++ StringAndStringBuilder/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace StringAndStringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoopString();
+
+            System.Console.ReadKey();
+
+            LoopStringBuilder();
+
+            System.Console.ReadKey();
+        }
+
+        public static void LoopString()
+        {
+            string data = string.Empty;
+            for (int i = 0; i < 1000; i++)
+            {
+                data = data + i;
+            }
+
+            System.Console.WriteLine("Loop String completed..");
+        }
+
+
+        public static void LoopStringBuilder()
+        {
+            StringBuilder data = new StringBuilder();
+            for (int i = 0; i < 1000; i++)
+            {
+                data.Append(i);
+            }
+
+            System.Console.WriteLine("LoopStringBuilder completed..");
+        }
+    }
+}
