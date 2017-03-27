d2608508515fed7140ef330a642b6ef7bdc62104
Linoy <linoymk@gmail.com>
2017-03-27 12:50:31 +0530

    Added KeyedCollection sample, which helps to create a dictionary without specifying the key.

---
+++ KeyedCollection/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace KeyedCollectionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary dic = new MyDictionary();
+            dic.Add(new Employee() { Id = 100, Name = "lmk" });
+            dic.Add(new Employee() { Id = 101, Name = "test" });
+
+            Employee e = dic[100];
+
+            Console.ReadKey();
+        }
+    }
+
+    public class MyDictionary : KeyedCollection<int, Employee>
+    {
+        protected override int GetKeyForItem(Employee item)
+        {
+            return item.Id;
+        }
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
