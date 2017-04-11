dcf31e0c24ff7f1c09f82ec3e6c024adddc2b51f
Linoy <linoymk@gmail.com>
2017-04-11 18:51:22 +0530

    ConfigureAwait(false) removing the context switching

--- ContextSwitching/Controllers/TestController.cs
+++ ContextSwitching/Controllers/TestController.cs
@@ -1,13 +1,24 @@
-﻿using System.Web.Mvc;
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace ContextSwitching.Controllers
 {
     public class TestController : Controller
     {
         // GET: Test
-        public ContentResult Index()
+        public async Task<ContentResult> Index()
         {
-            return Content("test");
+            var httpContext = System.Web.HttpContext.Current;
+            await GetStringAsync().ConfigureAwait(false);
+
+            var httpContextAfter = System.Web.HttpContext.Current;
+            return Content("testing");
+        }
+
+        public async Task GetStringAsync()
+        {
+            await Task.Delay(1000);
+            //return await Task.FromResult("data from async method");
         }
     }
 }
