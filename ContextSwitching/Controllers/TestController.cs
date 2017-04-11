22b5c0f32801b1da1ef540e9571b661e629abf59
Linoy <linoymk@gmail.com>
2017-04-11 18:57:01 +0530

    Methods separated for better context switch example.

--- ContextSwitching/Controllers/TestController.cs
+++ ContextSwitching/Controllers/TestController.cs
@@ -6,13 +6,40 @@ namespace ContextSwitching.Controllers
     public class TestController : Controller
     {
         // GET: Test
-        public async Task<ContentResult> Index()
+        public async Task<ContentResult> WithOutContextSwitch()
         {
+            string message = string.Empty;
             var httpContext = System.Web.HttpContext.Current;
-            await GetStringAsync().ConfigureAwait(false);
+            await GetStringAsync().ConfigureAwait(continueOnCapturedContext: false); // Original request context is not captured
 
             var httpContextAfter = System.Web.HttpContext.Current;
-            return Content("testing");
+            if (httpContextAfter == null)
+            {
+                message = "HttpContext is null after removing the context switching with ConfigureAwait(false)";
+            }
+            else
+            {
+                message = "HttpContext is not null";
+            }
+            return Content(message);
+        }
+
+        public async Task<ContentResult> WithContextSwitch()
+        {
+            string message = string.Empty;
+            var httpContext = System.Web.HttpContext.Current;
+            await GetStringAsync(); // Request context is captured by default.
+
+            var httpContextAfter = System.Web.HttpContext.Current;
+            if (httpContextAfter == null)
+            {
+                message = "HttpContext is null after removing the context switching with ConfigureAwait(false)";
+            }
+            else
+            {
+                message = "HttpContext is not null";
+            }
+            return Content(message);
         }
 
         public async Task GetStringAsync()
