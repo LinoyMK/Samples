ec9af59e2bf94e75d2605441a5c31cea5dc08193
Linoy <linoymk@gmail.com>
2017-04-26 14:38:42 +0530

    Project to check the lazy keyword

---
+++ LazyKeyword/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace LazyKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lazy<string> executionAndPublication = new Lazy<string>(() => GetException(), LazyThreadSafetyMode.ExecutionAndPublication); //--> Exception also cached..
+            bool isValueCreated = executionAndPublication.IsValueCreated;
+            var data = executionAndPublication.Value;
+            var data2 = executionAndPublication.Value;
+
+
+            Lazy<string> publication = new Lazy<string>(() => GetException(), LazyThreadSafetyMode.PublicationOnly); // --> Exceptions are not cached..
+            bool isValueCreated1 = publication.IsValueCreated;
+            var data3 = publication.Value;
+            var data4 = publication.Value;
+
+
+        }
+
+        private static string GetException()
+        {
+            throw new NotImplementedException("Tesiting exception caching");
+        }
+    }
+}
